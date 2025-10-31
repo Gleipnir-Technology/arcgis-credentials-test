eec26a2bd1aa88a59d185e430cd56d94abedca60
Eli Ribble <eli@gleipnir.technology>
2025-10-31 16:01:11 +0000

    Add ability to get client id and secret from the environment

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	client_id := os.Getenv("CLIENT_ID")
+	if client_id == "" {
+		log.Println("You must specify a non-empty CLIENT_ID")
+		os.Exit(1)
+	}
+	client_secret := os.Getenv("CLIENT_SECRET")
+	if client_secret == "" {
+		log.Println("You must specify a non-empty CLIENT_SECRET")
+		os.Exit(1)
+	}
+
 	log.Println("Starting...")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
